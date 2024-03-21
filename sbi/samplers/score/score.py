cc0761246c27f085bdb11542d842c398614c8fdc
paulf14 <paufu8@t-online.de>
2024-03-21 11:04:44 +0100

    score sampler

--- sbi/samplers/score/score.py
+++ sbi/samplers/score/score.py
@@ -2,30 +2,34 @@
 from typing import Callable, Any
 from tqdm.auto import tqdm
 import torch
-
+from sbi.inference.potentials import ScoreBasedPotential
 
 def score_based_sampler(
-    score_based_potential: Callable,
-    proposal: Any,
+    score_based_potential: ScoreBasedPotential,
+    proposal: Any, 
     drift: Callable,
     diffusion: Callable,
     ts: torch.Tensor,
+    dim_theta: int,
     num_samples: int = 1,
     show_progress_bars: bool = True,
+      
 ):
     r"""Returns a sampler for score-based methods.
 
     Args:
         score_based_potential: The score-based potential function.
-        proposal: The proposal distribution.
+        proposal: The proposal (noise) distribution .
         drift: The drift function of the SDE.
         diffusion: The diffusion function of the SDE.
 
     Returns:
         A sampler for score-based methods.
     """
-
-
+    iid2, batch, condition_shape = score_based_potential.x_o.shape
+    sample_shape = (num_samples, batch)
+    theta = proposal.sample(sample_shape)
+    delta_t = (1/ts.numel())
     pbar = tqdm(
         ts,
         disable=not show_progress_bars,
@@ -33,7 +37,10 @@ def score_based_sampler(
     )
 
     for t in pbar:
-        pass
+        theta = theta + (drift(input=theta, t=t)
+                        - (diffusion(input=theta, t=t)) ** 2
+                        * score_based_potential(theta=theta, diffusion_time=t)) * delta_t + diffusion(input=theta, t=t) * torch.randn((sample_shape, dim_theta)) * delta_t
+
             
             
 
